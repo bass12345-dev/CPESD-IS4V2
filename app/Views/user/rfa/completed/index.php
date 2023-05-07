1c2eb41b5a616c9bdc3e2011d0bba4fb978ac61b
bass12345-dev <manabobasil@gmail.com>
2023-05-06 17:08:38 -0700

    'update'

--- app/Views/user/rfa/completed/index.php
+++ app/Views/user/rfa/completed/index.php
@@ -33,64 +33,7 @@
     </div>     
 <?php echo view('includes/scripts.php') ?>   
 <script type="text/javascript">
-
-
-
-    
-
-
-    $('#rfa_completed_table').DataTable({
-
-            responsive: false,
-            "ordering": false,
-            "ajax" : {
-                        "url": base_url + 'api/user/get-user-completed-rfa',
-                        "type" : "POST",
-                        "dataSrc": "",
-            },
-             'columns': [
-                 {
-                data: "ref_number",
-                
-
-            },
-            {
-                data: "name",
-                
-
-            },
-              {
-                data: "address",
-               
-            },
-              {
-                data: "type_of_request_name",
-               
-
-            },
-              {
-                data: "type_of_transaction",
-               
-
-            },
-            
-              {
-                // data: "song_title",
-                data: null,
-                render: function (data, type, row) {
-                    return '<ul class="d-flex justify-content-center">\
-                                <li><a href="javascript:;" data-id="'+data['rfa_id']+'"   id="view_rfa_"  class="text-secondary action-icon"><i class="ti-eye"></i></a></li>\
-                                </ul>';
-                }
-
-            },
-           
-          ]
-        });
-
-
-
-// load_user_completed_rfa()
+$('#rfa_completed_table').DataTable({ responsive: false, "ordering": false, "ajax" : { "url": base_url + 'api/user/get-user-completed-rfa', "type" : "POST", "dataSrc": "", }, 'columns': [ { data: "ref_number", }, { data: "name", }, { data: "address", }, { data: "type_of_request_name", }, { data: "type_of_transaction", }, { data: null, render: function (data, type, row) { return '<ul class="d-flex justify-content-center">\ <li><a href="javascript:;" data-id="'+data['rfa_id']+'"   id="view_rfa_"  class="text-secondary action-icon"><i class="ti-eye"></i></a></li>\ </ul>'; } }, ] });
 </script>
 </body>
 </html>
